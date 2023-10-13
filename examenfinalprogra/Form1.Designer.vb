d3bde244212b8f83d136e839488169793c976c1e
admin <admin@ILSY>
2023-10-13 10:38:45 -0600

    login 1.0

--- examenfinalprogra/Form1.Designer.vb
+++ examenfinalprogra/Form1.Designer.vb
@@ -23,27 +23,79 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(358, 134)
+        Button1.BackColor = Color.Coral
+        Button1.Font = New Font("Times New Roman", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Button1.Location = New Point(349, 272)
         Button1.Name = "Button1"
-        Button1.Size = New Size(75, 23)
+        Button1.Size = New Size(82, 30)
         Button1.TabIndex = 0
-        Button1.Text = "Ilsy"
-        Button1.UseVisualStyleBackColor = True
+        Button1.Text = "Acceso"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.ScrollBar
+        TextBox1.Location = New Point(424, 161)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = SystemColors.ScrollBar
+        TextBox2.Location = New Point(424, 97)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Stencil", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(274, 101)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(84, 19)
+        Label1.TabIndex = 3
+        Label1.Text = "Usuario"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Stencil", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(259, 165)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(119, 19)
+        Label2.TabIndex = 4
+        Label2.Text = "Contraseña"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightCoral
         ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
         Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- examenfinalprogra/Form1.vb
+++ examenfinalprogra/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "Ilsy Ortíz" And TextBox2.Text = "123" Then
+            Form2.Show()
+            Hide()
+        End If
+        If TextBox1.Text = "Waldir Castillo" And TextBox2.Text = "456" Then
+            Form2.Show()
+            Hide()
+        End If
+    End Sub
 End Class
